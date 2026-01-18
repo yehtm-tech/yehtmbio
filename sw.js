3da453751c130bd90480cdb2c0aed6aa96d309a0
yehtm-tech <yehtm@smhs.hlc.edu.tw>
2026-01-18 09:39:21 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+// Service Worker - 用於快取啟動器，讓它能被安裝
+const CACHE_NAME = 'taipei-trip-v1';
+const urlsToCache = [
+  './',
+  './index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
